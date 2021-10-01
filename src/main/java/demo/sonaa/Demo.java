36424099efe9d1e985adcab43f177be553c31b59
phuxiteo3 <vanphuong0606@gmail.com>
2021-10-01 10:39:05 +0700

    first commit

---
+++ src/main/java/demo/sonaa/Demo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.sonaa;
+
+/**
+ *
+ * @author Dell
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
